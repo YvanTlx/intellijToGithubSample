de09fa2fec8e3885cfcb794a538b8823e06351f5
YvanTlx <31309002+yvantlx@users.noreply.github.com>
2024-03-30 21:10:41 +0200

    Class1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int number= 15;
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
+        System.out.print(number);
 
     }
 }
